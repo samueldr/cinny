540c3696ecd3828bf3b482b6aaa74ba33c2f166f
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-11-05 23:23:46 -0400

    [MOD:opinionated] Unneeded branding change
    Makes it obvious this is not stock standard cinny.

--- src/app/organisms/welcome/Welcome.jsx
+++ src/app/organisms/welcome/Welcome.jsx
@@ -10,8 +10,8 @@ function Welcome() {
     <div className="app-welcome flex--center">
       <div className="flex-v--center">
         <img className="app-welcome__logo noselect" src={CinnySvg} alt="Cinny logo" />
-        <Text className="app-welcome__heading" variant="h1">Welcome to Cinny</Text>
-        <Text className="app-welcome__subheading" variant="s1">Yet another matrix client</Text>
+        <Text className="app-welcome__heading" variant="h1">Welcome to Cinny(MOD)</Text>
+        <Text className="app-welcome__subheading" variant="s1">Not exactly like that other matrix client!</Text>
       </div>
     </div>
   );
